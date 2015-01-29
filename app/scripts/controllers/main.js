367e3ca9692d5e95085c10e4d7092c979f365281
Prashant Khanduri <pkhanduri@jawbone.com>
2015-01-28 21:16:09 -0800

    Adding more profile data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,14 +18,117 @@ angular.module('grayscaleApp')
 
     $scope.profile = {
       name: {
-        first: 'First',
-        middle: undefined,
-        last: 'Last'
+        first: 'Prashant',
+        last: 'Khanduri'
       },
       desc: {
         first : 'The first line of description',
         second : 'Second line of description'
       },
+      experience: [
+        {
+          fa_class: 'fa-spin fa-circle-o-notch',
+          title: 'Jawbone',
+          location: 'San Fransisco, CA',
+          dates: '2014 - current',
+          position: 'Programmer',
+          body: [
+            {
+              team_name: 'Insights & Intelligence team',
+              team_desc: 'The team is responsible for building an intelligent guide that helps our users make good health choices',
+              duties: [
+                'Working on bullding the core intelligence for the Smart Coach brand',
+                'Working on building the Insights system and the internal CRM system to manage the insight content'
+              ]
+            }
+          ]
+        },
+        {
+          fa_class: 'fa-comment-o',
+          title:'Hearsay Social',
+          location:'San Fransisco, CA',
+          dates:'2012 - 2014',
+          position:'Programmer',
+          body: [
+            {
+              team_name: 'Engagement team',
+              team_desc: 'Team\'s goal was to keep the user engagement up and unblock any onboarding issues (growing the monthly active users)',
+              duties: [
+                'Designed and developed a notification platform for our software',
+                'Worked on building the analytics and reporting framework'
+              ]
+            }
+          ]
+        },
+        {
+          fa_class: 'fa-windows',
+          title:'Microsoft Inc',
+          location:'Seattle, WA',
+          dates:'2009 - 2012',
+          position:'Software Developer',
+          body: [
+            {
+              team_name: 'Bing - Relevance',
+              team_desc: 'Team was responsible for ranking the top documents in Bing\'s algorithmic web search results (Top 100K queries)',
+              duties: [
+                'Developing new algorithms to extract ranking signals and features from petabytes of user browsing activity logs',
+                'Using data mining and machine learning techniques to optimize user satisfaction metrics'
+              ]
+            },
+            {
+              team_name: 'Windows Live',
+              team_desc: 'Worked with two separate groups (Service Management / Monetization) on delivering project critical components',
+              duties: [
+                'Implemented a service for syncing data from a system store to a DHCP server (for the server to hand out correct IP leases required for PXE booting)',
+                'Helped design the next generation monetization solution to better target Hotmail users'
+              ]
+            }
+          ]
+        },
+        {
+          fa_class: 'fa-code',
+          title: 'Internships',
+          location: 'Various Locations',
+          dates: '2004 - 2009',
+          position: 'Intern',
+          body: [
+            {
+              team_name: 'Microsoft Corporation',
+              team_desc: 'Windows Live Mesh - May 2009 – Aug 2009',
+              duties: [
+                'Created an assembly that provisioned a delegated authentication token management service'
+              ]
+            },
+            {
+              team_name: 'Amazon.com Inc',
+              team_desc: 'Personalization - Aug 2007 - Dec 2007',
+              duties: [
+                'Worked on a ranking mechanism to generate a tag set which describe the list recommended items'
+              ]
+            },
+            {
+              team_name: 'Sun Microsystems',
+              team_desc: 'Complex Systems Group - Jan 2006 - Apr 2006',
+              duties: [
+                'Participated in the design and development of a simulation infrastructure for the behavioral analysis of wireless sensor networks'
+              ]
+            },
+            {
+              team_name: 'Amazon.com Inc',
+              team_desc: 'Digital Media Technologies - May 2005 - Aug 2005',
+              duties: ['Worked on the variety of upgrades to the digital media pipeline (mainly the first internal REST client to the S3 service)'
+              ]
+            },
+            {
+              team_name: 'Epson Canada',
+              team_desc: 'Barcode Recognition - Jan 2004 - Apr 2004',
+              duties: [
+                'Developed a universal 2D barcode decoding application for hand-held image-scanners'
+              ]
+            }
+          ]
+        }
+      ],
       education: {
         institutes: [
           {
